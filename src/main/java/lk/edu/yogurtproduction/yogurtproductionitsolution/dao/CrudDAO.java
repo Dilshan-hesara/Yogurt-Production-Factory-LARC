3669f9c12791e67ff8c881c2e182055f3a2cae2b
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-01-21 19:05:59 +0530

    CrudDAO

---
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/dao/CrudDAO.java
@@ -0,0 +1,21 @@
+package lk.edu.yogurtproduction.yogurtproductionitsolution.dao;
+
+import lk.edu.yogurtproduction.yogurtproductionitsolution.dto.EmployeeDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface CrudDAO<T> {
+
+     ArrayList<T> getAll() throws SQLException, ClassNotFoundException ;
+
+     String getNextId() throws SQLException;
+
+     boolean save( T dto) throws SQLException ;
+
+     boolean update(T dto) throws SQLException, ClassNotFoundException;
+
+     boolean delete(String empId) throws SQLException ;
+
+     T findByID(String cmbEmpSelected) throws SQLException ;
+}
