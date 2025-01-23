8dbf992e2a692d62d07a13e6ad9ba63f9afa8192
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-01-23 11:52:18 +0530

    Packing BO

---
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/bo/custom/PackingBO.java
@@ -0,0 +1,25 @@
+package lk.edu.yogurtproduction.yogurtproductionitsolution.bo.custom;
+
+import lk.edu.yogurtproduction.yogurtproductionitsolution.bo.SuperBO;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.dto.PckingDto;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.entity.Employee;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface PackingBO extends SuperBO {
+
+    public String getNextId() throws SQLException;
+
+    public boolean save(PckingDto pckingDtos) throws SQLException;
+
+    public boolean savePacking(PckingDto pckingDtos) throws SQLException;
+
+    String getNextInvId() throws SQLException;
+
+    int AvQtyFromSelectProd_ID(String getSelectedProdId) throws SQLException;
+
+    ArrayList<String> getAllEmpIds() throws SQLException;
+
+    Employee findByEmployeeID(String cmbEmpSelected) throws SQLException;
+}
---
+++ src/main/java/lk/edu/yogurtproduction/yogurtproductionitsolution/bo/custom/impl/PackingBOImpl.java
@@ -0,0 +1,117 @@
+package lk.edu.yogurtproduction.yogurtproductionitsolution.bo.custom.impl;
+
+import lk.edu.yogurtproduction.yogurtproductionitsolution.bo.custom.PackingBO;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.dao.DAOFactroy;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.dao.custom.EmployeeDAO;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.dao.custom.InventroyDAO;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.dao.custom.PackingDAO;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.dao.custom.StockDAO;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.db.DBConnection;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.dto.PckingDto;
+import lk.edu.yogurtproduction.yogurtproductionitsolution.entity.Employee;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class PackingBOImpl implements PackingBO {
+
+        PackingDAO packingDAO = (PackingDAO) DAOFactroy.getInstance().getDAO(DAOFactroy.DAOType.PACKING);
+
+        InventroyDAO inventroyDAO = (InventroyDAO) DAOFactroy.getInstance().getDAO(DAOFactroy.DAOType.INVENTROY);
+
+        StockDAO stockDAO = (StockDAO) DAOFactroy.getInstance().getDAO(DAOFactroy.DAOType.STOCK);
+
+        EmployeeDAO employeeDAO = (EmployeeDAO) DAOFactroy.getInstance().getDAO(DAOFactroy.DAOType.EMPLOYEE);
+
+
+    public String getNextId() throws SQLException {
+
+        return packingDAO.getNextId();
+
+    }
+    public boolean save(PckingDto pckingDtos) throws SQLException {
+
+        return packingDAO.save(pckingDtos);
+
+    }
+
+    public boolean savePacking(PckingDto pckingDtos) throws SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            connection.setAutoCommit(false);
+
+//            boolean isRecpSaved = SQLUtil.execute(
+//                    "insert into packing values (?, ?, ?, ?, ?, ?, ?,?)",
+//                    pckingDtos.getPac_ID(),
+//                    pckingDtos.getProd_ID(),
+//                    pckingDtos.getPac_Type(),
+//                    pckingDtos.getPac_Desc(),
+//                    pckingDtos.getPac_Date(),
+//                    pckingDtos.getExpire_Date(),
+//                    pckingDtos.getQty(),
+//                    pckingDtos.getEmp_ID()
+//            );
+            boolean isRecpSaved = save(pckingDtos);
+
+            if (isRecpSaved) {
+
+
+                boolean isStockUpdated = stockDAO.saveStock(pckingDtos.getStockDTOS());
+                if (isStockUpdated) {
+                    boolean redusePackedQty = inventroyDAO.saveredusPackedQty(pckingDtos);
+                    if (redusePackedQty) {
+                        boolean isInventroyUpdated = inventroyDAO.saveInvetory(pckingDtos.getInventroyDTOS());
+                        if (isInventroyUpdated) {
+                            connection.commit();
+                            return true;
+                        }
+
+
+
+                    }
+
+                }
+
+            }
+
+            connection.rollback();
+            return false;
+        } catch(SQLException e){
+            connection.rollback();
+            e.printStackTrace();
+            return false;
+        } finally{
+            connection.setAutoCommit(true);
+        }
+
+    }
+
+    public  String getNextInvId() throws SQLException {
+
+        return inventroyDAO.getNextId();
+    }
+
+    public int AvQtyFromSelectProd_ID(String getSelectedProdId) throws SQLException {
+
+        return inventroyDAO.AvQtyFromSelectProd_ID(getSelectedProdId);
+    }
+
+
+   public ArrayList<String> getAllEmpIds() throws SQLException {
+
+        ArrayList<String> empIds = new ArrayList<>();
+        ArrayList<String> stringArrayList =employeeDAO.getAllEmpIds();
+
+        for (String string : stringArrayList){
+            empIds.add(string);
+        }
+        return empIds;
+  }
+
+   public Employee findByEmployeeID(String cmbEmpSelected) throws SQLException {
+        return employeeDAO.findByID(cmbEmpSelected);
+
+   }
+
+}
